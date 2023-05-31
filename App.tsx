141579bffb8d1b90d249f166fa61942fde0b02d4
emremutlu08 <mutluemre93@gmail.com>
2023-05-31 19:51:15 +0300

    feat(api): added generate script

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+// import ApiClient from "openapi-typescript-codegen";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    // const apiClient = new ApiClient();
+    // const response = await apiClient.get("/users");
+    // setUsers(response.data);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text>Open up App.tsx to start working on your app!</Text>

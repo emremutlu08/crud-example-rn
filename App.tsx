9d5d01afb56b8f153d6211fabe01836976d1e7f9
emremutlu08 <mutluemre93@gmail.com>
2023-05-31 22:16:01 +0300

    feat(api): started to use SimpleCrudService

--- App.tsx
+++ App.tsx
@@ -1,23 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
-// import ApiClient from "openapi-typescript-codegen";
+import { SimpleCrudService } from "./generated";
 
 export default function App() {
-  const [users, setUsers] = useState([]);
-
   const fetchUsers = async () => {
-    // const apiClient = new ApiClient();
-    // const response = await apiClient.get("/users");
-    // setUsers(response.data);
+    try {
+      const response = await SimpleCrudService.listUsersUsersGet();
+      console.log(response, "response");
+    } catch (error) {
+      console.error(error, "error");
+    }
   };
 
   useEffect(() => {
+    console.log("useEffect");
     fetchUsers();
   }, []);
 
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="flex-1 items-center justify-center bg-blue-100 h-full">
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>

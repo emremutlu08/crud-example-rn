521d3f96fb5b57ecc894f3a3aefa643101a35fd2
emremutlu08 <mutluemre93@gmail.com>
2023-06-03 02:17:48 +0300

    feat(form): added form validator with yup

--- src/components/form/student-info-form.tsx
+++ src/components/form/student-info-form.tsx
@@ -1,4 +1,5 @@
 import { Formik } from 'formik';
+import * as yup from 'yup'; // Import yup for validation
 
 import { SimpleCrudService, type UserModel } from '../../../generated';
 import { toast } from '../../helpers/toast';
@@ -31,9 +32,16 @@ export default function StudentInfoForm(props: IStudentInfoFormProps): JSX.Eleme
     };
   }
 
+  const validationSchema = yup.object().shape({
+    name: yup.string().required('Name is required'),
+    age: yup.number().required('Age is required').positive('Age must be a positive number'),
+    hometown: yup.string().required('Hometown is required'),
+  });
+
   return (
     <Formik
       initialValues={initialFormValues}
+      validationSchema={validationSchema}
       onSubmit={(values) => {
         const sendableValues: UserModel = { ...values, age: Number(values.age) };
         (async () => {
@@ -46,10 +54,11 @@ export default function StudentInfoForm(props: IStudentInfoFormProps): JSX.Eleme
           onSubmit();
         })().catch((err) => {
           console.error(err, 'err');
+          toast('An error occurred. Please try again.');
         });
       }}
     >
-      {({ handleChange, handleBlur, handleSubmit, values }) => {
+      {({ handleChange, handleBlur, handleSubmit, values, touched }) => {
         return (
           <>
             <StyledInput
@@ -57,6 +66,7 @@ export default function StudentInfoForm(props: IStudentInfoFormProps): JSX.Eleme
               onChangeText={handleChange('name')}
               onBlur={handleBlur('name')}
               value={values.name}
+              error={touched?.name}
             />
             <StyledInput
               label="Age"
@@ -64,16 +74,18 @@ export default function StudentInfoForm(props: IStudentInfoFormProps): JSX.Eleme
               onBlur={handleBlur('age')}
               value={values.age}
               keyboardType="numeric"
+              error={touched?.age}
             />
             <StyledInput
               label="Hometown"
               onChangeText={handleChange('hometown')}
               onBlur={handleBlur('hometown')}
               value={values.hometown}
+              error={touched?.hometown}
             />
 
             <StyledButton
-              className="flex flex-row items-center justify-between bg-green-600  rounded-md mt-2"
+              className="flex flex-row items-center justify-between bg-green-600 rounded-md mt-2"
               onPress={() => {
                 handleSubmit();
               }}

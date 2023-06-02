aa32d506394bd33073801f3e2c833327e67abb65
emremutlu08 <mutluemre93@gmail.com>
2023-06-02 21:50:43 +0300

    feat(appbar): implement appbar component

---
+++ src/components/header/appbar.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+
+import { Appbar } from 'react-native-paper';
+
+import DrawerContent from './drawer-content';
+import PortalModal from '../ui/modal/portal-modal';
+
+export default function AppBar(): JSX.Element {
+  const [visible, setVisible] = React.useState(false);
+
+  const showModal = (): void => {
+    setVisible(true);
+  };
+  const hideModal = (): void => {
+    setVisible(false);
+  };
+
+  return (
+    <>
+      <PortalModal visible={visible} onDismiss={hideModal} isLeftDrawer>
+        <DrawerContent />
+      </PortalModal>
+      <Appbar.Header>
+        <Appbar.Action icon="forwardburger" onPress={showModal} />
+
+        <Appbar.Content title="" />
+        <Appbar.Action icon="bell" onPress={() => {}} />
+      </Appbar.Header>
+    </>
+  );
+}
